4cf39347b8eaeda0f95442edf03580cefb3846b2
parisa kalaki <parisakalaki@parisas-MacBook-Air.local>
2023-09-20 12:37:05 +0300

    update app.js

--- app/app.js
+++ app/app.js
@@ -1,16 +1,68 @@
-import { serve } from "./deps.js";
-import { sql } from "./database.js";
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+import * as addressService from "./services/addressService.js";
 
-const logNames = async () => {
-  const result = await sql`SELECT * FROM names`;
-  console.log(result);
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-const handleRequest = (request) => {
-  console.log(`Request to ${request.url}`);
-  logNames();
-  return new Response("Hello world!");
+const redirectTo = (path) => {
+  return new Response(`Redirecting to ${path}.`, {
+    status: 303,
+    headers: {
+      "Location": path,
+    },
+  });
 };
 
-console.log("Launching server on port 7777");
+const create = async (request) => {
+  const formData = await request.formData();
+
+  const sender = formData.get("sender");
+  const message = formData.get("message");
+
+  await addressService.create(sender, message);
+  return redirectTo("/");
+};
+
+const handleRequest = async (request) => {
+  const url = new URL(request.url);
+  if (request.method === "GET" && url.pathname === "/") {
+    const data = {
+      messages: await addressService.findAll(),
+    };
+    return new Response(await renderFile("index.eta", data), responseDetails);
+  } else if (request.method === "POST") {
+    return await create(request);
+  }
+};
+
 serve(handleRequest, { port: 7777 });
+
+// const deleteAddress = async (request) => {
+//   const url = new URL(request.url);
+//   const parts = url.pathname.split("/");
+//   const id = parts[2];
+//   await addressService.deleteById(id);
+
+//   return redirectTo("/");
+// };
+
+// const listAddresses = async () => {
+//   const data = {
+//     addresses: await addressService.findAll(),
+//   };
+
+//   return new Response(await renderFile("index.eta", data), responseDetails);
+// };
+
+// const measure = async () => {
+//   const data = {
+//     addresses: await addressService.measure(),
+//   };
+//   return new Response(await renderFile("index.eta", data), responseDetails);
+// };
